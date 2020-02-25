03fc2ab3f37d2321c5c53b075a30ff8df0eb85b8
evascarpa <55817681+evascarpa@users.noreply.github.com>
2020-02-25 15:40:06 -0300

    usr logueado

--- src/main/java/com/dh/BaproClubEntregable/controller/HomeController.java
+++ src/main/java/com/dh/BaproClubEntregable/controller/HomeController.java
@@ -1,13 +1,20 @@
 package com.dh.BaproClubEntregable.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.dh.BaproClubEntregable.model.Usuario;
+import com.dh.BaproClubEntregable.repository.UsuarioJpaRepository;
 
 @Controller
 public class HomeController {
+	
+	@Autowired 
+	private UsuarioJpaRepository usuarioJpaRepository;
 
 	@GetMapping("index") //index es igual a comillas vacias
 	public String getIndex() {
@@ -19,10 +26,13 @@ public class HomeController {
 	return "MiMuro";
 	}
 
-	@PostMapping("login")
-	public String login(Usuario usr) {
+	@PostMapping("/login")
+	public String login(Usuario usr , Model model) {
+	Usuario usrLogueado = usuarioJpaRepository.findUserByMail(usr.getEmail());
+	model.addAttribute("usuario", usrLogueado);	
 	return "nuevoPerfil";
 	}
+	
 
 	
 	@GetMapping("/contacto")
--- src/main/java/com/dh/BaproClubEntregable/repository/UsuarioJpaRepository.java
+++ src/main/java/com/dh/BaproClubEntregable/repository/UsuarioJpaRepository.java
@@ -1,11 +1,14 @@
 package com.dh.BaproClubEntregable.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.dh.BaproClubEntregable.model.Usuario;
 
 @Repository
 public interface UsuarioJpaRepository extends JpaRepository<Usuario, Integer>{
+	@Query("SELECT u FROM Usuario u WHERE u.email = ?1")
+	Usuario findUserByMail(String Mail);
 	
 }
