ce3b1e6307a6c986339f304cfd280fd6b8b017a5
evascarpa <55817681+evascarpa@users.noreply.github.com>
2020-03-04 02:38:06 -0300

    cierre de sesion

--- src/main/java/com/dh/BaproClubEntregable/controller/CuentaController.java
+++ src/main/java/com/dh/BaproClubEntregable/controller/CuentaController.java
@@ -2,19 +2,38 @@ package com.dh.BaproClubEntregable.controller;
 
 
 
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dh.BaproClubEntregable.model.Publicacion;
+import com.dh.BaproClubEntregable.model.Usuario;
 import com.dh.BaproClubEntregable.repository.CuentaJpaRepository;
+import com.dh.BaproClubEntregable.repository.UsuarioJpaRepository;
 
-@RestController
+@Controller
 public class CuentaController {
 	
 	@Autowired
 	private CuentaJpaRepository cuentaJpaRepository;
+	@Autowired
+	private UsuarioJpaRepository usuarioJpaRepository; 
 
-
+	@PostMapping("cerrarSesion")
+	public String cerrarSesion(Usuario usr , Model model, HttpServletRequest request) {	
+		HttpSession misession= request.getSession(true);
+		misession.invalidate();
+		return "index";
+	
+		}
+		
 	
 	
 	
